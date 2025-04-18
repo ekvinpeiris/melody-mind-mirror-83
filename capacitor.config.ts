f3373193992471c38b61f351a30310ddfb94215b
Rojith Peiris <rojithonline@gmail.com>
2025-04-18 22:30:50 +0530

    initiate capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.appzone.midiplayer',
+  appName: 'midiplayer',
+  webDir: 'dist'
+};
+
+export default config;
